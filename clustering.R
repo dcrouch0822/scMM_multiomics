2517f0c1312394d5b40fb4c1855c385674e72fa3
Danielle C <dccroucher@gmail.com>
2022-04-06 10:23:07 -0400

    Update and rename multires_clustering.R to clustering.R

--- clustering.R
+++ clustering.R
@@ -114,7 +114,7 @@ print("********************")
 print("")
 StopWatchStart$LoadPackages  <- Sys.time()
 
-suppressMessages(library(scran))
+#suppressMessages(library(scran))
 suppressMessages(library(Matrix))
 suppressMessages(library(scales))
 suppressMessages(library(viridis))
@@ -707,12 +707,12 @@ print("")
 
 StopWatchStart$tSNE <- Sys.time()
 print(paste0("Running tSNE..."))
-seurat.obj <- RunTSNE(seurat.obj, reduction = "harmony", dims = 1:pc.use, verbose = FALSE, max_iter = 2000, check_duplicates = FALSE)
+seurat.obj <- RunTSNE(seurat.obj, dims = 1:pc.use, verbose = FALSE, max_iter = 2000, check_duplicates = FALSE)
 StopWatchEnd$tSNE <- Sys.time()
 
 StopWatchStart$UMAP <- Sys.time()
 print(paste0("Running UMAP..."))
-seurat.obj <- RunUMAP(seurat.obj, reduction = "harmony", dims = 1:pc.use, verbose = FALSE)
+seurat.obj <- RunUMAP(seurat.obj, dims = 1:pc.use, verbose = FALSE)
 StopWatchEnd$UMAP <- Sys.time()
 
 print(paste0("Add coordinates to metadata..."))
@@ -722,9 +722,7 @@ seurat.obj <- AddMetaData(seurat.obj,
 seurat.obj <- AddMetaData(seurat.obj,
                           metadata = data.frame(seurat.obj@reductions$tsne@cell.embeddings)
                           )
-seurat.obj <- AddMetaData(seurat.obj,
-                          metadata = data.frame(seurat.obj@reductions$harmony@cell.embeddings)
-                          )
+
 
 #########################################
 # Clustering
@@ -746,7 +744,6 @@ seurat.obj@meta.data <- seurat.obj@meta.data[, !colnames(seurat.obj@meta.data) %
 #default in seurat is to use k = 20
 print(paste0("Constructing SNN Graph with ", pc.use, " PCs..."))
 seurat.obj <- FindNeighbors(seurat.obj,
-                            reduction = "harmony",
                             dims = 1:pc.use,
                             verbose = TRUE
                            )
@@ -766,7 +763,7 @@ res.range <- round(seq(minResolution,
 print(res.range)
 
 seurat.obj <- FindClusters(seurat.obj,
-                           resolution = res.range,
+                           resolution = 0.4, #picking this for now - can adjust later
                            #method = "igraph", #better for large data
                            algorithm = 1, #default Louvian algorithm,
                            verbose = FALSE,
@@ -794,278 +791,6 @@ StopWatchEnd$Clustering <- Sys.time()
 
 
 #########################################
-# DE Marker Genes
-#########################################
-
-if (optimalCluster == TRUE){
-  
-print("")
-print("********************")
-print("Differential Gene Expression Analysis")
-print(Sys.time())
-print("********************")
-print("")
-StopWatchStart$MarkerGenes <- Sys.time()
-
-resolutions <- colnames(seurat.obj@meta.data)[grep("_res", colnames(seurat.obj@meta.data))]
-#print(resolutions)
-
-markers <- list()
-
-for (i in 1:length(resolutions)){
-
-    #set cell identity to match resolution
-    seurat.obj <- SetIdent(seurat.obj, value = resolutions[i])
-    print(paste0(i,
-                 "/",
-                 length(resolutions),
-                 "...",
-                 resolutions[i],
-                 " (",
-                 #length(unique(seurat.obj@meta.data[ ,resolutions[i]])),
-                 length(unique(Idents(seurat.obj))),
-                 " clusters)"
-                ))
-
-    DE.outs <- FindAllMarkers(seurat.obj,
-                              test.use = "wilcox",
-                              only.pos = TRUE,
-                              min.pct = 0.25, #only test genes in 25% of both populations
-                              logfc.threshold = 0.25, #log-fold change threshold
-                              verbose = FALSE,
-                              pseudocount.use = 1/nrow(seurat.obj@meta.data),
-                              return.thresh = 0.01 #pvalue to return genes
-                             )
-
-    DE.outs$resolution <- as.numeric(gsub("Seurat_cluster_res.", "", resolutions[i]))
-    DE.outs$sample <- fileName
-    rownames(DE.outs) <- NULL
-    markers[[resolutions[i]]] <- DE.outs
-    rm(DE.outs) #help to conserve memory
-    gc()
-
-}
-
-#combine gene marker lists from all solutions into one file
-#save as RDS and csv
-print("Merging DE results....")
-markers <- do.call("rbind", markers)
-rownames(markers) <- NULL
-
-#remove genes that are above FDR cutoff
-print(paste0("Filtering DE genes....FDR=", fdrCutoff))
-markers_fdr <- markers[markers$p_val_adj <= fdrCutoff, ]
-print(paste0("Removed ", nrow(markers) - nrow(markers_fdr), " genes...."))
-
-StopWatchEnd$MarkerGenes <- Sys.time()
-
-
-
-} else if (optimalCluster == FALSE){
-
-  #if you are not picking optimal cluster solution
-  #use the middle clustering solution within range as the one for plotting
-  middle.res <- res.range[length(res.range)/2]
-  print(paste0("DE testing not required if optimal clustering not performed"))
-
-}
-
-###### HERE IS WHERE WE NEED TO DETERMIN IF LOWER RES NEEDS
-###### TO BE CALCULATED
-###### TABL
-
-#########################################
-# Sil Width
-#########################################
-
-if (optimalCluster == TRUE){
-
-print("")
-print("********************")
-print("Calculate Silhouette Width")
-print("********************")
-print(Sys.time())
-print("")
-StopWatchStart$SilhouetteWidth <- Sys.time()
-#?silhouette() in cluster package
-#this will give a value for each cell
-#https://rpkgs.datanovia.com/factoextra/reference/fviz_silhouette.html
-#calculate sil values using pca matrix
-#same matrix used for input to clustering
-
-pca.dist <- as.matrix(seurat.obj@reductions$pca@cell.embeddings[ ,1:pc.use])
-pca.dist <- dist(pca.dist)
-
-silhouette.width <- list()
-
-for(i in 1:length(resolutions)){
-
-    print(paste0(i,
-                 "/",
-                 length(resolutions),
-                 "...",
-                 resolutions[i]
-                ))
-
-    cl <- seurat.obj@meta.data[ ,resolutions[i]] #beware this adds +1 to cluster numbers
-
-    sil <- cluster::silhouette(as.integer(cl), pca.dist)
-    sil <- data.frame(sil[, 1:ncol(sil)])
-    sil$cluster <- sil$cluster - 1
-    sil$resolution <- as.numeric(gsub("Seurat_cluster_res.", "", resolutions[i]))
-    sil$cell <- rownames(pca.dist)
-
-    silhouette.width[[resolutions[i]]] <- sil
-    rm(sil) #help to conserve memory
-    gc()
-
-}
-
-print("Merging silhouette widths across solutions....")
-silhouette.width <- do.call("rbind", silhouette.width)
-rownames(silhouette.width) <- NULL
-head(silhouette.width)
-
-StopWatchEnd$SilhouetteWidth <- Sys.time()
-
-
-
-#########################################
-# Select Clustering Solution
-#########################################
-
-print("")
-print("********************")
-print("Select Optimal Clustering Solution")
-print(Sys.time())
-print("********************")
-print("")
-StopWatchStart$OptimalSolution <- Sys.time()
-
-print(paste0("Minimum ",
-             deGeneCutoff,
-             " DE genes/cluster after FDR filtering....")
-     )
-
-#extract the number of DE genes per cluster
-deGenes <- table(markers_fdr$cluster, markers_fdr$resolution)
-deGenes <-  reshape2::melt(deGenes)
-colnames(deGenes) <- c("cluster", "resolution", "nDEGenes")
-
-#filter on cluster number
-
-ress <- unique(markers_fdr$resolution)
-newdata <- list()
-for (i in 1:length(ress)){
-
-    clust.name <- paste0("Seurat_cluster_res.", ress[i])
-    #print(clust.name)
-    #subset <- seurat.obj@meta.data[ ,grep(clust.name, colnames(seurat.obj@meta.data))]
-    subset <- seurat.obj@meta.data[ ,clust.name]
-    num.clust <- length(unique(subset))
-    #print(num.clust)
-    a <- deGenes[deGenes$resolution == ress[i], ]
-    a <- a[1:num.clust, ]
-    newdata[[i]] <- a
-
-}
-
-deGenes <- do.call(rbind, newdata)
-
-#deGenes <- deGenes %>% drop_na()
-#colnames(deGenes) <- c("cluster", "resolution", "nDEGenes")
-
-print("Cluster for each resolution with lowest # of DE genes...")
-clust.min <- data.frame(deGenes %>% group_by(resolution) %>% top_n(n = -1, wt = nDEGenes))
-clust.min$Pass_DE <- clust.min$nDEGenes >= deGeneCutoff
-clust.min <- clust.min[!duplicated(clust.min$resolution), ]
-print(dim(clust.min))
-print("")
-
-possible.solutions <- clust.min$resolution[clust.min$nDEGenes >= deGeneCutoff]
-print(paste0("Resolutions that pass # DE gene cutoff....", paste(possible.solutions, collapse = ", ")))
-
-#print(paste0("Calculate min and max....", paste(possible.solutions, collapse = ", ")))
-print("Calculate median silhuoette width for resolutions....")
-
-silhouette.width$res_cluster <- paste0("res",
-                                       silhouette.width$resolution,
-                                       "_",
-                                       silhouette.width$cluster
-                                      )
-
-##first calculate sil width for each cluster
-#sil_aggr <- aggregate(silhouette.width[, "sil_width"],
-#                      list(silhouette.width$res_cluster),
-#                      mean
-#                     )
-#colnames(sil_aggr) <- c("res_cluster", "sil_avg")
-#sil_aggr$resolution <- sapply(strsplit(sil_aggr$res_cluster,"_"), `[`, 1)
-#sil_aggr <- sil_aggr[order(sil_aggr$resolution), ]
-
-# then average the sil width across all clusters
-# this gives cluster even weight in the calculation
-#sil_avg <- aggregate(sil_aggr[, "sil_avg"],
-#                      list(sil_aggr$resolution),
-#                      mean
-#                     )
-
-#clust.min$Sil_Avg <- sil_avg$x
-
-#### MEDIAN CALCULATIONS
-
-sil_aggr <- aggregate(silhouette.width[, "sil_width"],
-                      list(silhouette.width$res_cluster),
-                      median
-                     )
-colnames(sil_aggr) <- c("res_cluster", "sil_median")
-sil_aggr$resolution <- sapply(strsplit(sil_aggr$res_cluster,"_"), `[`, 1)
-sil_aggr <- sil_aggr[order(sil_aggr$resolution), ]
-
-sil_med <- aggregate(sil_aggr[, "sil_median"],
-                      list(sil_aggr$resolution),
-                      median
-                     )
-clust.min$Sil_median <- sil_med$x
-
-#### PICK OPTIMAL SOLUTION
-print(clust.min)
-opt.solution <- clust.min[clust.min$Pass_DE == TRUE, ]
-opt.solution <- opt.solution[opt.solution$Sil_median == max(opt.solution$Sil_median),  ] #has to pass DE test
-
-# if multiple solutions are tied...
-# pick the highest resolution
-if(nrow(opt.solution) > 1){
-    opt.solution <- opt.solution[opt.solution$resolution == max(opt.solution$resolution), ]
-}
-
-#add optimal resolution to metadata and assign ident
-seurat.obj@meta.data$Optimal_res <- opt.solution$resolution
-new.ident <- colnames(seurat.obj@meta.data)[grep(opt.solution$resolution, colnames(seurat.obj@meta.data))]
-seurat.obj <- SetIdent(seurat.obj, value = new.ident)
-
-print(paste0("Optimal solution is.....Resolution = ", opt.solution$resolution))
-
-deGenes$res_cluster <- paste0("res", deGenes$resolution, "_", deGenes$cluster)
-deGenes <- deGenes[order(deGenes$res_cluster), ]
-deGenes$sil_median <- sil_aggr$sil_median
-optimal.file <- paste0("./data/",fileName, "_DE_Silhouette.csv")
-print(optimal.file)
-write.csv(deGenes, file = optimal.file)
-
-StopWatchEnd$OptimalSolution <- Sys.time()
-
-} else if (optimalCluster == FALSE){
-
-  #if you are not picking optimal cluster solution
-  #use the middle clustering solution within range as the one for plotting
-  middle.res <- res.range[length(res.range)/2]
-  print(paste0("Picking middle of resolution range as optimal clustering solution: ", middle.res))
-  seurat.obj@meta.data$Optimal_res <- middle.res
-
-}
-
-#########################################
 # Save Data
 #########################################
 
@@ -1077,53 +802,51 @@ print("********************")
 print("")
 StopWatchStart$SaveData <- Sys.time()
 
-print("Raw Counts....")
-raw.count.dir <- paste0("./data/", fileName, "_rawCounts")
-raw <- Matrix(seurat.obj@assays$RNA@counts, sparse = TRUE)
-write10xCounts(path = raw.count.dir,
-               x = raw
-               )
+#print("Raw Counts....")
+#raw.count.dir <- paste0("./data/", fileName, "_rawCounts")
+#raw <- Matrix(seurat.obj@assays$RNA@counts, sparse = TRUE)
+#write10xCounts(path = raw.count.dir,
+#               x = raw
+#               )
 
-print("Normalized Counts....")
-norm.count.dir <- paste0("./data/", fileName, "_normCounts")
-norm.count <- Matrix(seurat.obj@assays$RNA@data, sparse = TRUE)
-write10xCounts(path = norm.count.dir,
-               x = norm.count
-               )
+#print("Normalized Counts....")
+#norm.count.dir <- paste0("./data/", fileName, "_normCounts")
+#norm.count <- Matrix(seurat.obj@assays$RNA@data, sparse = TRUE)
+#write10xCounts(path = norm.count.dir,
+#               x = norm.count
+#               )
 
 print("Meta Data....")
 meta <- data.frame(seurat.obj@meta.data)
-meta.file <- paste0("./data/", fileName, "_metaData.csv")
+meta.file <- paste0("./data/", sampleNames, "_metaData.csv")
 write.csv(meta, file = meta.file)
-meta.file.2 <- paste0("./data/", fileName, "_metaData.rds")
-saveRDS(meta, file = meta.file.2)
 
-print("Scree Plot Information....")
-scree.file <- paste0("./data/", fileName, "_scree.csv")
-write.csv(pca, file = scree.file)
+#print("Scree Plot Information....")
+#scree.file <- paste0("./data/", fileName, "_scree.csv")
+#write.csv(pca, file = scree.file)
 
-print("PCA....")
-pca.file <- paste0("./data/", fileName, "_PCA.rds")
-pca.mat <- seurat.obj@reductions$pca
-saveRDS(pca.mat, file = pca.file)
+#print("PCA....")
+#pca.file <- paste0("./data/", fileName, "_PCA.rds")
+#pca.mat <- seurat.obj@reductions$pca
+#saveRDS(pca.mat, file = pca.file)
 
-if (optimalCluster == TRUE){
-print("DE Markers....")
-DE.file <- paste0("./data/", fileName, "_DE_markers.csv")
-write.csv(markers, file = DE.file)
-DE.file.2 <- paste0("./data/", fileName, "_DE_markers_FDR_filered.csv")
-write.csv(markers_fdr, file = DE.file.2)
+#if (optimalCluster == TRUE){
+#print("DE Markers....")
+#DE.file <- paste0("./data/", fileName, "_DE_markers.csv")
+#write.csv(markers, file = DE.file)
+#DE.file.2 <- paste0("./data/", fileName, "_DE_markers_FDR_filered.csv")
+#write.csv(markers_fdr, file = DE.file.2)
 
 
-print("Silhouette Width....")
-sil.file <- paste0("./data/", fileName, "_silhouette_widths.rds")
-saveRDS(silhouette.width, file = sil.file)
-sil.file.2 <- paste0("./data/", fileName, "_clustering_dashboard.csv")
-write.csv(deGenes, file = sil.file.2)
-}
+#print("Silhouette Width....")
+#sil.file <- paste0("./data/", fileName, "_silhouette_widths.rds")
+#saveRDS(silhouette.width, file = sil.file)
+#sil.file.2 <- paste0("./data/", fileName, "_clustering_dashboard.csv")
+#write.csv(deGenes, file = sil.file.2)
+#}
 
 print("Saving Seurat Object....")
-seurat.file <- paste0("./data/", fileName, "_seurat.rds")
+seurat.file <- paste0("./data/", sampleNames, "_seurat.rds")
 saveRDS(seurat.obj, file = seurat.file)
 
 StopWatchEnd$SaveData <- Sys.time()
@@ -1149,21 +872,9 @@ if(outputPlots == TRUE){
     if(class(marker.genes) == "data.frame"){
 
         print("Adding Marker Genes to Metadata...")
-        gene.index <- as.character(marker.genes$GENE) %in% rownames(seurat.obj@assays$RNA@data)
-        genes <- as.character(marker.genes$GENE)[gene.index]
-        subset <- seurat.obj@assays$RNA@data[genes, ]
-        subset <- data.frame(data.matrix(t(subset)))
-        seurat.obj <- AddMetaData(seurat.obj,
-                                  metadata = subset
-                                 )
-
-        if(FALSE %in% gene.index){
-            print("The following genes are not in the dataset: ")
-            print(as.character(marker.genes$GENE)[!gene.index])
-        }
-
-
-
+        
+        seurat.obj <- AddMetaData(seurat.obj, FetchData(seurat.obj, marker.genes$GENE))
+        
     }
 
     meta <- data.frame(seurat.obj@meta.data)
@@ -1172,18 +883,18 @@ if(outputPlots == TRUE){
 
     print("1/6....QC plots")
 
-    QC1 <- ggplot(meta, aes(x=nGene, y=nUMI)) +
+    QC1 <- ggplot(meta, aes(x=nGene_RNA, y=nUMI_RNA)) +
           geom_point(size = 0.6, alpha = 0.7) +
           theme(legend.position="none") + theme_classic()
     QC1 <- ggMarginal(QC1, type="histogram")
 
-    QC2 <- ggplot(meta, aes(x=nGene, y=percent.mt)) +
+    QC2 <- ggplot(meta, aes(x=nGene_RNA, y=percent.mt)) +
           geom_point(size = 0.6, alpha = 0.7) +
           theme(legend.position="none") + theme_classic()
     QC2 <- ggMarginal(QC2, type="histogram")
 
 
-    qc.plots <- paste0("./figures/",fileName, "_QC.pdf")
+    qc.plots <- paste0("./figures/",sampleNames, "_QC.pdf")
     print(qc.plots)
     pdf(qc.plots, width = 10, height = 5)
     print(ggarrange(QC1, QC2, ncol = 2, nrow = 1))
